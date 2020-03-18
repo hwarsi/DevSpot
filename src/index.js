b0b9c945ba56e999e858c5f1079d256eafb0ee3f
Faraz <farazfkm@gmail.com>
2020-03-17 20:04:08 -0400

    new

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import './styles/app.scss';
+import './static/styles/app.scss';
 
-import UserRegistration from './Components/UserRegistration'
+import UserRegistration from './Components/Registration/UserRegistration'
 import NavBar from './Components/Navbar/Navbar'
 
 function MainApp() {
