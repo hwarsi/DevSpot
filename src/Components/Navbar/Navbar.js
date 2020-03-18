bf03c88d1343f2d7ea5391bd74df554fff2f6e92
Faraz <farazfkm@gmail.com>
2020-03-17 20:00:18 -0400

    new

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,22 +1,25 @@
-import React, {Component} from './node_modules/react';    
+import React, {Component} from 'react';    
 
 
-class Navbar extends Component {
-    render() {
-    return
-      <div>
-        <SearchBar/>
-      </div>
-    }
-  }
-  class SearchBar extends Component {
-    render() {
-      return( <form>
+class SearchBar extends Component {
+  render() {
+    return( 
+      <form>
         <input type="text"/>
-        </form>
-      );
-    }
+      </form>
+    );
   }
+}
 
-  export default NavBar
+class NavBar extends Component {
+    render() {
+      return (
+        <div>
+          <SearchBar/>
+        </div>
+      );
+  }
+}
+
+export default NavBar;
   
