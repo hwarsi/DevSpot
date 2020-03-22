692728c50b91d3158277bc82b3013673f60971f3
Faraz <farazfkm@gmail.com>
2020-03-21 20:33:01 -0400

    new

--- src/Components/Registration/UserRegistration.js
+++ src/Components/Registration/UserRegistration.js
@@ -1,34 +1,63 @@
 import React, {Component} from 'react';
 
+import { Button, Popover, Tooltip, DatePicker, Input, Radio } from 'antd';
+import { QuestionCircleTwoTone } from '@ant-design/icons'
+
 class UserRegistration extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {};
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit = () => {
-        console.log('Hey');
     }
 
     render() {
+
+        const birthday = ("You can change who sees your gender on your profile later. Select Custom to choose another gender, or if you'd rather not say.");
+        const gender = ("You can change who sees your gender on your profile later. Select Custom to choose another gender, or if you'd rather not say.");
+
         return (
-            <div className="FormContainer" >
+            <div className="RegistrationForm" >
                 <h4>Sign Up</h4>
-                <p>It's quick and easy</p>
-                <input type="text" name="firstName" />
-                <input type="text" name="lastName" />
-                <input type="text" name="contact" />
-                <input type="text" name="password" />
-                <div>
-                    
-                </div>
-                <div className="drow">
-                    <div className="dcol">
-                       <button onClick={this.handleSubmit}>Hey</button>
+                <p>It's quick and easy.</p>
+
+                <div className="FormContainer" >
+                    <div className='name'>
+                        <Input size="large" placeholder="First name" />
+                        <Input size="large" placeholder="Last name" />
+                    </div>
+                    <Input size="large" placeholder="Mobile number or email" />
+                    <Input.Password size="large" placeholder="New password" />
+
+                    <div className="birthday">
+                        <DatePicker />
+                        <Popover content={birthday} trigger="click">
+                            <Tooltip placement="topLeft" title="Click for more information" arrowPointAtCenter>
+                                <QuestionCircleTwoTone />
+                            </Tooltip>
+                        </Popover>
+                    </div>
+
+                    <div className="gender">
+                    <Radio.Group>
+                        <Radio value={'Male'}>Male</Radio>
+                        <Radio value={"Female"}>Female</Radio>
+                    </Radio.Group>
+                        <Popover content={gender} trigger="click">
+                            <Tooltip placement="topLeft" title="Click for more information" arrowPointAtCenter>
+                                <QuestionCircleTwoTone />
+                            </Tooltip>
+                        </Popover>
+                    </div>
+
+                    <div className="terms">
+                        By clicking Sign Up, you agree to our Terms, Data Policy and Cookies Policy. You may receive SMS Notifications from us and can opt out any time.
+                    </div>
+
+                    <div>
+                    <Button type="primary" size={"Large"}>
+                        Sign Up
+                    </Button>
                     </div>
                 </div>
             </div>
