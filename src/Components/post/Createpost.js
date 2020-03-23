6632f170704d897d859c3d970127af0f32fa0b6c
hwarsi <hwarsi@g.clemson.edu>
2020-03-23 11:45:25 -0400

    new

---
+++ src/Components/post/Createpost.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';  
+
+
+class Createpost extends Component {
+    constructor(props) {
+        super(props);
+        const {  Input  } = antd;
+        const {  UserOutlined  } = icons;
+
+
+    }
+            render() {
+                return(
+                <div className="example-input">
+                    <Input size="large" placeholder="large size" prefix={<UserOutlined />} />
+                </div>,
+                         mountNode,
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <meta name="theme-color" content="#000000">
+                  </head>
+                  <body>
+                    <div id="container" style="padding: 24px" />
+                )
+            }
+}
---
+++ src/Components/post/postnavbar/Postnavbar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';  
+
+
+class PostNavbar extends Component {
+    constructor(props) {
+      super(props);
+  
+    }
+  
+      render() {
+        return (
+          <div className="PostNavBar">
+        
+  
+          </div>
+  
+        );
+    }
+  }
+  
+  
+  export default NavBar;
+    
